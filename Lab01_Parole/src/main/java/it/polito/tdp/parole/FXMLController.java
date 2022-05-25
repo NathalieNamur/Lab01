50cadf8a9af78536ff1866317933c6d3e888c772
Nathalie <Nathalie@192.168.1.56>
2022-05-25 10:13:31 +0200

    Lab01 Completato

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -1,47 +1,112 @@
 package it.polito.tdp.parole;
 
-import it.polito.tdp.parole.model.Parole;
+import java.util.List;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import it.polito.tdp.parole.model.Parole;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+
 public class FXMLController {
+
+	Parole elenco; 
 	
-	Parole elenco ;
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
+	@FXML
     private TextField txtParola;
-
-    @FXML
-    private Button btnInserisci;
-
-    @FXML
+	
+	@FXML
+	 private Button btnInserisci;
+	
+	@FXML
     private TextArea txtResult;
-
+	
+	@FXML
+	private TextArea txtPerformance;
+	
+	@FXML
+	private Button btnReset;
+	
     @FXML
-    private Button btnReset;
+    private Button btnCancella;
 
+    
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
-    }
+    	
+    	//1.ACQUISIZIONE E CONTROLLO DATI:
+    	String parola = txtParola.getText();
+    	
+    	if(parola.equals("")) {
+    		txtResult.setText("ERRORE: inserire una parola");
+    		return;
+    	}
+    	
+    	//2.ESECUZIONE DELL'OPERAZIONE (MODEL): 
+    	double start = System.nanoTime();
+    	elenco.addParola(parola);
+    	double stop = System.nanoTime();
+    	
+    	//3.VISUALIZZAZIONE/AGGIORNAMENTO DEL RISULTATO:
+    	txtResult.clear();
+    	
+    	List<String> paroleInserite = elenco.getElenco();
+    	for(String p : paroleInserite)
+    		txtResult.appendText(p.toString()+"\n");
+    	
+    	txtPerformance.clear();
+    	txtPerformance.setText("Operazione inserimento parola: " + (stop-start)/1e9 + " sec");
+    	
+    	txtParola.clear();
 
+    }
+    
+    
     @FXML
-    void doReset(ActionEvent event) {
-    	// TODO
+    void doRest(ActionEvent event) {
+    	
+    	elenco.reset();
+    	
+    	txtResult.clear();
+    	txtPerformance.clear();
+
+    }
+    
+    
+    @FXML
+    void doCancella(ActionEvent event) {
+    
+    	String parolaSelezionata = txtResult.getSelectedText();
+    	
+    	if(parolaSelezionata.equals("")) {
+    		txtResult.setText("ERRORE: selezionare una parola");
+    		txtPerformance.clear();
+    		return;
+    	}
+    	
+    	
+    	double start = System.nanoTime();
+    	elenco.removeParola(parolaSelezionata);
+    	double stop = System.nanoTime();
+    	
+    	
+    	txtResult.clear();
+    	
+    	List<String> paroleRestanti = elenco.getElenco();
+    	for(String p : paroleRestanti)
+    		txtResult.appendText(p.toString()+"\n");
+    	
+    	txtPerformance.clear();
+    	txtPerformance.setText("Operazione cancellazione parola: " + (stop-start)/1e9 + " sec");
+    	
+    	txtParola.clear();
+
     }
 
+   
+    
     @FXML
     void initialize() {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
@@ -51,4 +116,5 @@ public class FXMLController {
 
         elenco = new Parole() ;
     }
+
 }
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,42 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Parole {
-		
+	
+	//ATTRIBUTI:
+	private List<String> listaParole;
+	
+	
+	//COSTRUTTORE:
 	public Parole() {
-		//TODO
+		
+		listaParole = new ArrayList<String>();
+		
+		//In alternativa LinkedList:
+		//listaParole = new ArrayList<String>();
 	}
 	
+	
+	//METODI:
 	public void addParola(String p) {
-		//TODO
+		listaParole.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+
+		Collections.sort(listaParole);
+		return listaParole;
 	}
 	
 	public void reset() {
-		// TODO
+		listaParole.clear();
+	}
+	
+	public void removeParola(String p) {
+		listaParole.remove(p);
 	}
 
 }
